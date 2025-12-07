925241b50565ca928dea0281377783f83bf8fe7c
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-12-07 16:38:24 +0530

    Made Regions for different things

--- Zombie Game/Assets/Scripts/PlayerInput.cs
+++ Zombie Game/Assets/Scripts/PlayerInput.cs
@@ -1,12 +1,38 @@
+using System;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    #region MainVariables
+
     private Vector3 _input;
 
+    #endregion
+
+    #region Update
+
+    private void Update()
+    {
+        GatherInput();
+        Move();
+    }
+
+    #endregion
+
+
+    #region Functions
+    
     void GatherInput()
     {
         _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
     }
-    
+
+    void Move()
+    {
+        
+    }
+
+    #endregion
+
+
 }
