3f5e9f69949abbb3a0bbe7b4074c03bf59b4822e
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2025-12-22 23:48:16 +0530

    animation setup tomorrow

--- Zombie Game/Assets/Scripts/PlayerInputParent.cs
+++ Zombie Game/Assets/Scripts/PlayerInputParent.cs
@@ -52,7 +52,6 @@ public abstract class PlayerInputParent : MonoBehaviour
         _currentMovement.x = _Input.x;
         _currentMovement.z = _Input.y;
         _isMovementPressed = _Input.x != 0 || _Input.y != 0;
-        
     }
     protected void GatherInputOnRotation(InputAction.CallbackContext context)
     {
