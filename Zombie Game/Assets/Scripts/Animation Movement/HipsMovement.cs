2e30a82b7a567f4b75b6639187e7875868df3b5a
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-01-29 00:20:29 +0530

    stupid changes

--- Zombie Game/Assets/Scripts/Animation Movement/HipsMovement.cs
+++ Zombie Game/Assets/Scripts/Animation Movement/HipsMovement.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class HipsMovement : MonoBehaviour
 {
+    #region Variables
+
+    private Quaternion hipsCurrentRotation;
+    private float hipsYRoration = 14.305f;
+
+    #endregion
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,10 @@ public class HipsMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if (Input.GetKeyDown(KeyCode.W))
+        // {
+        //     hipsCurrentRotation = transform.rotation;
+        //     transform.rotation = new Quaternion(hipsCurrentRotation.x,hipsYRoration,hipsCurrentRotation.z,hipsCurrentRotation.w);
+        // }
     }
 }
