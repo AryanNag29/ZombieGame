fbd872425dc493295f17f7fd2ad27cc3f6a67366
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-01-30 23:15:14 +0530

    shitty script

---
+++ Zombie Game/Assets/Scripts/Player Script/Bullet.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+    public class Bullet : MonoBehaviour {
+
+
+        private void Awake() {
+            Destroy(gameObject, 3f);
+        }
+
+        private void Update() {
+            float moveSpeed = 10f;
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+
+        // private void OnTriggerEnter2D(Collider2D collider2D) {
+        //     if (collider2D.gameObject.TryGetComponent(out Enemy enemy)) {
+        //         enemy.GetComponent<HealthSystem>().Damage(30);
+        //         DestroySelf();
+        //     }
+        
+
+        // private void DestroySelf() {
+        //     Destroy(gameObject);
+        // }
+
+    }
+    
+
