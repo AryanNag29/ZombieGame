f0917b827b7fbafdd5e1d7abeb3db8b6b53985d5
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-02-06 03:39:59 +0530

    bullet implementation is still left

--- Zombie Game/Assets/Scripts/Player Script/Gun.cs
+++ Zombie Game/Assets/Scripts/Player Script/Gun.cs
@@ -3,39 +3,63 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Gun : MonoBehaviour
 {
-   #region Variables
-
-   [SerializeField] private bool addBulletSpread = true;
-   [SerializeField] private Vector3 bulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
-   [SerializeField] private ParticleSystem shootingSystem;
-   [SerializeField] private Transform bulletSpawn;
-   [SerializeField] private ParticleSystem impactParticleSystem;
-   [SerializeField] private float shootingDelay = 0.05f;
-   [SerializeField] private LayerMask _mask;
-   private Animator _animator;
-   private float lastShotTime;
-
-   #endregion
-
-   #region Awake
-
-   private void Awake()
-   {
-      _animator = GetComponent<Animator>();
-   }
-
-   #endregion
-
-
-   #region Functions
-
-   public void Attack()
-   {
-      if (lastShotTime + shootingDelay < Time.time)
-      {
-         //not gonna use object pulling
-      }
-   }
-
-   #endregion
+   // #region Variables
+   //
+   // [SerializeField] private bool addBulletSpread = true;
+   // [SerializeField] private Vector3 bulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
+   // [SerializeField] private ParticleSystem shootingSystem;
+   // [SerializeField] private Transform bulletSpawn;
+   // [SerializeField] private ParticleSystem impactParticleSystem;
+   // [SerializeField] private float shootingDelay = 0.05f;
+   // [SerializeField] private LayerMask _mask;
+   // private Animator _animator;
+   // private float lastShotTime;
+   //
+   // #endregion
+   //
+   // #region Awake
+   //
+   // private void Awake()
+   // {
+   //    _animator = GetComponent<Animator>();
+   // }
+   //
+   // #endregion
+   //
+   //
+   // #region Functions
+   //
+   // public void Attack()
+   // {
+   //    if (lastShotTime + shootingDelay < Time.time)
+   //    {
+   //       //not gonna use object pulling
+   //       shootingSystem.Play();
+   //       Vector3 direction = getDirection();
+   //
+   //       if (Physics.Raycast(bulletSpawn.position, direction, out RaycastHit hit, float.MaxValue, _mask))
+   //       {
+   //          TrailRenderer trail = Instantiate(bulletTrail, bulletSpawn.position, Quaternion.identity);
+   //          
+   //          StartCoroutine(spawnTrail(trail,hit));
+   //          
+   //          lastShotTime = Time.time;
+   //       }
+   //    }
+   // }
+   //
+   // private Vector3 getDirection()
+   // {
+   //    Vector3 direction = transform.forward;
+   //    if (addBulletSpread)
+   //    {
+   //       direction += new Vector3(
+   //          Random.Range(-bulletSpreadVariance.x,bulletSpreadVariance.x)
+   //          Random.Range(-bulletSpreadVariance.y,bulletSpreadVariance.y
+   //          Random.Range(-bulletSpreadVariance.z,bulletSpreadVariance.z
+   //          )
+   //    }
+   // }
+   //
+   // #endregion
 }
--- Zombie Game/Assets/Scripts/Player Script/PlayerAction.cs
+++ Zombie Game/Assets/Scripts/Player Script/PlayerAction.cs
@@ -20,7 +20,7 @@ public class PlayerAction : MonoBehaviour
     private void OnAttack()
     {
         onattack = true;
-        gun.Attack();
+        // gun.Attack();
     }
 
     #endregion
