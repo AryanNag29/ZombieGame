035cb2458656e00034f0871ec2bc3264490a5c19
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-02-21 15:49:40 +0530

    Update gun config file
    Creating CreateTrail funtion

--- Zombie Game/Assets/Scripts/Player Script/GunScriptObject.cs
+++ Zombie Game/Assets/Scripts/Player Script/GunScriptObject.cs
@@ -11,6 +11,7 @@ public class GunScriptObject : MonoBehaviour
     public string name;
     public GameObject modelPrefab;
     public Vector3 spawnPosition;
+    public Vector3 spawnRotation;
 
     public ShootConfigrationObject shootConfig;
     public TrailConfig trailConfig;
@@ -31,9 +32,18 @@ public class GunScriptObject : MonoBehaviour
     {
         this.ActiveMonobehaviour = ActiveMonobehaviour;
         lastShootTime = 0f; // in editor this will not be properly rest, in built it's fine
-        
-        
+        trailPool = new ObjectPool<TrailRenderer>(CreateTrail);
+        model = Instantiate(modelPrefab);
+        model.transform.SetParent(Parent,false);
+        model.transform.localPosition = spawnPosition;
+        model.transform.localRotation = Quaternion.Euler(spawnRotation);
 
+        shootSystem = model.GetComponentInChildren<ParticleSystem>();
+    }
+
+    private TrailRenderer CreateTrail()
+    {
+        
     }
 
     #endregion
