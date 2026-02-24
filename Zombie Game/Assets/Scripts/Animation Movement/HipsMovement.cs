1537b7ad58ccc009f5f6e11b41dfa0b3475887f5
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-02-24 22:39:31 +0530

    NameSpaceing every script as NameSpace - ZombieGame

--- Zombie Game/Assets/Scripts/Animation Movement/HipsMovement.cs
+++ Zombie Game/Assets/Scripts/Animation Movement/HipsMovement.cs
@@ -1,31 +1,36 @@
 using UnityEngine;
 
-public class HipsMovement : MonoBehaviour
+namespace ZombieGame
 {
-    #region Variables
-
-    private Quaternion hipsCurrentRotationLocal;
-    private Quaternion hipsCurrentRotationWorld;
-    private float hipsYRoration = 14.305f;
-
-    #endregion
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public class HipsMovement : MonoBehaviour
     {
-        
+        #region Variables
+
+        private Quaternion hipsCurrentRotationLocal;
+        private Quaternion hipsCurrentRotationWorld;
+        private float hipsYRoration = 14.305f;
+
+        #endregion
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            // hipsCurrentRotationLocal = transform.rotation;
+            // Matrix4x4 mworld = transform.localToWorldMatrix;
+            // hipsCurrentRotationWorld = mworld.rotation;
+            // // Debug.Log(hipsCurrentRotationWorld.eulerAngles);
+            // if (Input.GetKeyDown(KeyCode.W))
+            // { 
+            //     transform.rotation = Quaternion.Euler(hipsCurrentRotationWorld.x,hipsYRoration,hipsCurrentRotationWorld.z);
+            //     Debug.Log(transform.rotation.eulerAngles);
+        }
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        // hipsCurrentRotationLocal = transform.rotation;
-        // Matrix4x4 mworld = transform.localToWorldMatrix;
-        // hipsCurrentRotationWorld = mworld.rotation;
-        // // Debug.Log(hipsCurrentRotationWorld.eulerAngles);
-        // if (Input.GetKeyDown(KeyCode.W))
-        // { 
-        //     transform.rotation = Quaternion.Euler(hipsCurrentRotationWorld.x,hipsYRoration,hipsCurrentRotationWorld.z);
-        //     Debug.Log(transform.rotation.eulerAngles);
-        }
 }
-
