209e20922434a57d08bf36bfcf0eb00e4e639a3a
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-03-09 23:19:19 +0530

    Removed surface manager

--- Zombie Game/Assets/surface-manager-main/Assets/Scripts/ImpactType.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ZombieGame
-{
-    [CreateAssetMenu(menuName = "Impact System/Impact Type", fileName = "ImpactType")]
-    public class ImpactType : ScriptableObject
-    {
-    }
-}
