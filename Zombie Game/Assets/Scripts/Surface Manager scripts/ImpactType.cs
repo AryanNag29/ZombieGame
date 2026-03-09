88cfffef3ae86be4b58f9de4985083d4fd53cc46
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-03-10 02:07:37 +0530

    refactor

--- Zombie Game/Assets/Scripts/Surface Manager scripts/ImpactType.cs
+++ Zombie Game/Assets/Scripts/Surface Manager scripts/ImpactType.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Impact System/Impact Type", fileName = "ImpactType")]
-public class ImpactType : ScriptableObject
+namespace ZombieGame
 {
+    [CreateAssetMenu(menuName = "Impact System/Impact Type", fileName = "ImpactType")]
+    public class ImpactType : ScriptableObject
+    {
 
+    }
 }
--- Zombie Game/Assets/Scripts/Surface Manager scripts/SurfaceEffect.cs
+++ Zombie Game/Assets/Scripts/Surface Manager scripts/SurfaceEffect.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Impact System/Surface Effect", fileName = "SurfaceEffect")]
-public class SurfaceEffect : ScriptableObject
+namespace ZombieGame
 {
-    public List<SpawnObjectEffect> SpawnObjectEffects = new List<SpawnObjectEffect>();
-    public List<PlayAudioEffect> PlayAudioEffects = new List<PlayAudioEffect>();
+    [CreateAssetMenu(menuName = "Impact System/Surface Effect", fileName = "SurfaceEffect")]
+    public class SurfaceEffect : ScriptableObject
+    {
+        public List<SpawnObjectEffect> SpawnObjectEffects = new List<SpawnObjectEffect>();
+        public List<PlayAudioEffect> PlayAudioEffects = new List<PlayAudioEffect>();
+    }
 }
