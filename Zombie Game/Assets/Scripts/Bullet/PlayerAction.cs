1e490ba2697ed2dfb72beec46f208f86f4c0e079
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-03-17 23:51:25 +0530

    Moved player action

---
+++ Zombie Game/Assets/Scripts/Bullet/PlayerAction.cs
@@ -0,0 +1,79 @@
+using System;
+using UnityEngine;
+
+namespace ZombieGame
+{
+    public class PlayerAction : MonoBehaviour
+    {
+        #region References
+
+        [SerializeField] protected Gun gun;
+        [SerializeField] protected PlayerInputParent playerInput;
+        [SerializeField] protected InputSystem_Actions controls;
+
+        #endregion
+
+        #region Variables
+
+        protected bool onattack;
+
+        #endregion
+
+        #region Funtions
+
+        private void OnAttack()
+        {
+            onattack = true;
+            gun.Attack();
+            gun.DealDamage();
+        }
+
+        private void OnStopAttacking()
+        {
+            onattack = false;
+            gun.StopAttacking();
+        }
+
+        #endregion
+
+        #region Update
+
+        private void Update()
+        {
+            if (playerInput._shoot)
+            {
+                OnAttack();
+                gun.DealDamage();
+            }
+            else if (!playerInput._shoot || onattack)
+            {
+                OnStopAttacking();
+            }
+        }
+
+        #endregion
+
+        #region Awake
+
+        private void Awake()
+        {
+            controls = new InputSystem_Actions();
+        }
+
+        #endregion
+
+        #region Enable/Disable
+
+        private void OnEnable()
+        {
+            controls.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            controls.Player.Disable();
+        }
+
+        #endregion
+    }
+}
