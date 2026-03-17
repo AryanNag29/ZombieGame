062c33936f931ef7a1947e8f9d1649e5872875a2
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-03-18 00:00:55 +0530

    PlayerGunSelector is done

--- Zombie Game/Assets/Scripts/New Bulet manager script/PlayerGunSelector.cs
+++ Zombie Game/Assets/Scripts/New Bulet manager script/PlayerGunSelector.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ZombieGame
 {
+    [DisallowMultipleComponent]
     public class PlayerGunSelector : MonoBehaviour
     {
 
@@ -13,8 +15,29 @@ namespace ZombieGame
         #endregion
 
         #region Variables
+
         [Space] [Header("Runtime Filled")] public GunScriptObject ActiveGun;
-        
+        #endregion
+
+
+        #region Start
+
+        private void Start()
+        {
+            GunScriptObject gun = Guns.Find(gun => gun.gunType == Gun);
+            if(gun == null)
+            {
+                Debug.LogError($"No GunScriptableObject Found For GunType: {gun}");
+                return;
+            }
+
+            ActiveGun = gun;
+            gun.Spawn(GunParent, this);
+            
+            //IK not using now
+
+        }
+
         #endregion
     }
 }
